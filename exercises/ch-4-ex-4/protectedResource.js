b5d0b0521f0203d59a511924b67d86b7411db28c
Li Yong <li.yong@forgerock.com>
2020-04-11 16:58:08 +0800

    Done

--- exercises/ch-4-ex-4/protectedResource.js
+++ exercises/ch-4-ex-4/protectedResource.js
@@ -70,14 +70,24 @@ var bobFavorites = {
 	'music': ['baroque', 'ukulele', 'baroque ukulele']
 };
 
-app.get('/favorites', getAccessToken, requireAccessToken, function(req, res) {
-	
+app.get('/favorites', getAccessToken, requireAccessToken, function (req, res) {
+
 	/*
 	 * Get different user information based on the information of who approved the token
 	 */
-	
-	var unknown = {user: 'Unknown', favorites: {movies: [], foods: [], music: []}};
-	res.json(unknown);
+	if (req.access_token.user == 'alice') {
+		res.json({ user: 'Alice', favorites: aliceFavorites });
+	} else if (req.access_token.user == 'bob') {
+		res.json({ user: 'Bob', favorites: bobFavorites });
+	} else {
+		var unknown = {
+			user: 'Unknown', favorites: {
+				movies: [], foods: [],
+				music: []
+			}
+		};
+		res.json(unknown);
+	}
 
 });
 
