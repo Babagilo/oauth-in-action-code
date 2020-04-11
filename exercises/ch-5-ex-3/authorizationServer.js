661b5e22c64e094c24ce31d31d3843f05eedb0b5
Li Yong <li.yong@forgerock.com>
2020-04-12 02:24:56 +0800

    scope support added

--- exercises/ch-5-ex-3/authorizationServer.js
+++ exercises/ch-5-ex-3/authorizationServer.js
@@ -35,7 +35,7 @@ var clients = [
 		/*
 		 * Add a set of allowed scopes for this client
 		 */
-
+		,"scope": "foo bar"
 	}
 ];
 
@@ -69,7 +69,16 @@ app.get("/authorize", function(req, res){
 		 * Validate that the set of scopes the client is requesting 
 		 * aligns with the set of scopes the client is registered for.
 		 */
-		
+		var rscope = req.query.scope ? req.query.scope.split(' ') : undefined;
+		var cscope = client.scope ? client.scope.split(' ') : undefined;
+		if (__.difference(rscope, cscope).length > 0) {
+			var urlParsed = buildUrl(req.query.redirect_uri, {
+				error: 'invalid_scope'
+			});
+			res.redirect(urlParsed);
+			return;
+		}
+
 		var reqid = randomstring.generate(8);
 		
 		requests[reqid] = req.query;
@@ -77,7 +86,7 @@ app.get("/authorize", function(req, res){
 		/*
 		 * Send the requested scopes to the approval page for rendering
 		 */
-		res.render('approve', {client: client, reqid: reqid });
+		res.render('approve', {client: client, reqid: reqid, scope: rscope });
 		return;
 	}
 
@@ -102,8 +111,17 @@ app.post('/approve', function(req, res) {
 			/*
 			 * Make sure the approved scopes from the form are allowed for this client
 			 */
-
-
+			let rscope = getScopesFromForm(req.body)
+			console.log('rscope: ' + rscope);
+			var client = getClient(query.client_id);
+			var cscope = client.scope ? client.scope.split(' ') : undefined;
+			if (__.difference(rscope, cscope).length > 0) {
+				var urlParsed = buildUrl(query.redirect_uri, {
+					error: 'invalid_scope'
+				});
+				res.redirect(urlParsed);
+				return;
+			}
 			var code = randomstring.generate(8);
 			
 			// save the code and request for later
@@ -112,7 +130,7 @@ app.post('/approve', function(req, res) {
 			 * Save the approved scopes as part of this object
 			 */
 			
-			codes[code] = { request: query };
+			codes[code] = { request: query , 'scope': rscope};
 		
 			var urlParsed = buildUrl(query.redirect_uri, {
 				code: code,
@@ -190,8 +208,8 @@ app.post("/token", function(req, res){
 				var access_token = randomstring.generate();
 				var refresh_token = randomstring.generate();
 
-				nosql.insert({ access_token: access_token, client_id: clientId });
-				nosql.insert({ refresh_token: refresh_token, client_id: clientId });
+				nosql.insert({ access_token: access_token, client_id: clientId, 'scope':code.scope });
+				nosql.insert({ refresh_token: refresh_token, client_id: clientId , 'scope':code.scope});
 
 				console.log('Issuing access token %s', access_token);
 
@@ -199,7 +217,7 @@ app.post("/token", function(req, res){
 				 * Return scopes as part of the token response
 				 */
 				
-				var token_response = { access_token: access_token, token_type: 'Bearer',  refresh_token: refresh_token };
+				var token_response = { access_token: access_token, token_type: 'Bearer',  refresh_token: refresh_token, 'scope':code.scope.join(' ') };
 
 				res.status(200).json(token_response);
 				console.log('Issued tokens for code %s', req.body.code);
@@ -269,7 +287,7 @@ var buildUrl = function(base, options, hash) {
 };
 
 var decodeClientCredentials = function(auth) {
-	var clientCredentials = new Buffer(auth.slice('basic '.length), 'base64').toString().split(':');
+	var clientCredentials = Buffer.from(auth.slice('basic '.length), 'base64').toString().split(':');
 	var clientId = querystring.unescape(clientCredentials[0]);
 	var clientSecret = querystring.unescape(clientCredentials[1]);	
 	return { id: clientId, secret: clientSecret };
